2303142d831f9d48b088dfab254de0e9701efc64
Bianca Balabenute <balabenute.bianca@gmail.com>
2023-09-15 13:42:56 -0300

    changes

---
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,25 @@
+package com.example.cardapio.food;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Food {
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
+
+    public Food(FoodRequestDTO data){
+        this.title = data.title();
+        this.image = data.image();
+        this.price = data.price();
+    }
+}
